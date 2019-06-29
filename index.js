a70ccf0f21300562d81a846329c2e761238cb4e9
yannis300307 <52321382+yannis300307@users.noreply.github.com>
2019-06-29 11:02:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ bot.on('message', function (message) {
 				.addField("Les commandes : \n\n >server \n >help \n >invit")
 				.setFooter(".")
 			message.channel.send(help)
-			console.log('--------------------------------------')
+			console.log('--------------------------------------' + <@your_id_here>)
 			console.log('commande >help utilée par :')
 			console.log('--------------------------------------')
 	}
